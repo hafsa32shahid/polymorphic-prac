53613774ccc581b0768f63d59f17a7257d9c97dd
hafsa32shahid <farihafshahid@gmail.com>
2025-10-19 05:57:05 -0700

    Second commit

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+    ];
+
+     public function images(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'users' => UserController::class,
+    'posts' => PostController::class,
+]);
